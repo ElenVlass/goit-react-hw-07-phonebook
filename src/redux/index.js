a960f9bd6d1b99841d3e623c715cd86683fa933b
Elena Vlasenko <olefelena@gmail.com>
2021-07-22 21:44:21 +0300

    change naming file phoneBook-selectors.js to contacts-selectors.js

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,4 @@
 export { default as phoneBookReducer } from "./phoneBook-reduser";
-export { default as phoneBookSelectors } from "./phoneBook-selectors";
+export { default as phoneBookSelectors } from "./contacts-selectors";
 export * from "./phoneBook-actions";
 export { default as operations } from "./phoneBook-operations";
